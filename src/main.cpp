602433af5a5e9757137603856ec530719b4ae928
DrAta <adescudero93@gmail.com>
2018-10-26 16:40:37 +0200

    Added borders to rendered QR!

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 using qrcodegen::QrCode;
 
-#define PIX_PER_MODULE 3
+#define PIX_PER_MODULE 10
 #define BORDER 4
 
 static void QRToPixels(const QrCode &qr, unsigned char* rgb, int QrImageWidth, int QrImageHeight);
@@ -26,8 +26,6 @@ int main() {
   int QrImageHeight = (qr.getSize() + BORDER * 2) * PIX_PER_MODULE;
 
   int QrImageSize = QrImageWidth * QrImageHeight;
-  std::cout << QrImageSize << std::endl;
-
 
   // allocates memory for pixels array
   unsigned char* rgb = (unsigned char *) malloc (QrImageSize);
@@ -37,54 +35,40 @@ int main() {
 
   free(rgb);
 
+  std::cout << qr.toSvgString(4) << std::endl;
+
   return 0;
 }
 
 static void QRToPixels(const QrCode &qr, unsigned char* rgb, int QrImageWidth, int QrImageHeight) {
 
-  int count = 0;
   int QrImageSize = QrImageWidth * QrImageHeight;
+
   // initialize array with white pixels
-  for(int i = 0; i < QrImageSize - 3; i = i+3) {
+  for(int i = 0; i < QrImageSize; i = i+3) {
     rgb[i] = 255;
     rgb[i+1] = 255;
     rgb[i+2] = 255;
-    count += 3;
   }
 
-  std::cout << "Tamaño de la array: " << count << std::endl;
-
   //x columns, y rows
-	for (int y = -BORDER; y < qr.getSize() + BORDER; y++) {
-		for (int x = -BORDER; x < qr.getSize() + BORDER; x++) {
+	for (int y = 0; y < qr.getSize(); y++) {
+		for (int x = 0; x < qr.getSize(); x++) {
       if (qr.getModule(x, y)) {
-          paintModule(qr, x, y, rgb);
+          paintModule(qr, x + BORDER, y + BORDER, rgb);
       }
 		}
 	}
-
-  std::cout << count << std::endl;
 
   stbi_write_bmp("demo.bmp", (qr.getSize() + BORDER * 2) * PIX_PER_MODULE, (qr.getSize() + BORDER * 2) * PIX_PER_MODULE, 3, rgb);
 }
 
 static void paintModule(const QrCode &qr, int x, int y, unsigned char* rgb) {
     for(int i = y * PIX_PER_MODULE; i < y * PIX_PER_MODULE + PIX_PER_MODULE; i++) {
-        std::cout << "Vuelta i: " << i << std::endl;
         for (int j = x * PIX_PER_MODULE * 3; j < (x * PIX_PER_MODULE * 3 + (PIX_PER_MODULE * 3)); j = j + 3) {
-            std::cout << "Vuelta j: " << j << std::endl;
-            std::cout << "Painted: \t(" << j << ", " << i << ")" << std::endl;
-            std::cout << "\t\t\t(" << j+1 << ", " << i << ")" << std::endl;
-            std::cout << "\t\t\t(" << j+2 << ", " << i << ")" << std::endl;
-            std::cout << std::endl;
 
             // index to 1D
             int index = (i * ((qr.getSize() + BORDER * 2) * PIX_PER_MODULE) * 3) + j;
-
-            //std::cout << "DEBUG" << std::endl;
-            std::cout << "Indice: " << index << std::endl;
-            std::cout << "Indice: " << index + 1 << std::endl;
-            std::cout << "Indice: " << index + 2 << std::endl;
 
             // paint black pixels
             rgb[index] = 0;
